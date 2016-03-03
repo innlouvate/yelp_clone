783d8be9415c95f401c06dd00a2448195a4f39d9
innlouvate <l.j.franklin85@googlemail.com>
2016-03-03 15:33:15 +0000

    adding star ratings

--- spec/features/review_feature_spec.rb
+++ spec/features/review_feature_spec.rb
@@ -3,11 +3,19 @@ require_relative 'feature_helper'
 
 feature 'reviews' do
 
+  # scenario 'displays an average rating for all reviews' do
+  #   sign_up_and_create_restaurant
+  #   leave_review('So so', '3')
+  #   leave_review('Great', '5')
+  #   expect(page).to have_content('Average rating: 4')
+  # end
+
   scenario 'displays an average rating for all reviews' do
     sign_up_and_create_restaurant
-    leave_review('So so', '3')
-    leave_review('Great', '5')
-    expect(page).to have_content('Average rating: 4')
+    leave_review('so so', '3')
+    leave_review('Great!', '5')
+    expect(page).to have_content('Average rating: ★★★★☆')
   end
+
 
 end
